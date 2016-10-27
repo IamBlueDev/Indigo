5b2d6f92569ae73ce1d2ecd9a7ff44a80ef754d7
PC-R10500192748\mdxtech <mokshay_241294@hotmail.com>
2016-10-27 17:59:35 +0100

    added 321

--- app/src/main/java/bluedev/indigo/Main.java
+++ app/src/main/java/bluedev/indigo/Main.java
@@ -9,6 +9,6 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // This is a test231321321
+        // This is a test231321321bvyc 9i
     }
 }
