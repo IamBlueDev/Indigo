2bc21058711f219e8d0cabfa2fef0749765c5c86
PC-R10500192742\mdxtech <tanweerkbaig@gmail.com>
2016-10-27 18:20:02 +0100

    Changes Format
    Changes:
    [Filename]
    + things added
    - things removed
    
    Example
    [Main.class]
    + added function 'ClickHandler'
    + fixed button not working
    - removed comment from line 32

--- app/src/main/java/bluedev/indigo/Main.java
+++ app/src/main/java/bluedev/indigo/Main.java
@@ -2,6 +2,23 @@ package bluedev.indigo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+/*
+
+Changes Format
+
+Changes:
+[Filename]
++ things added
+- things removed
+
+Example
+[Main.class]
++ added function 'ClickHandler'
++ fixed button not working
+- removed comment from line 32
+
+ */
+
 
 public class Main extends AppCompatActivity {
 
@@ -9,6 +26,6 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // This is a test231321321bvyc 9i
+        // hello!
     }
 }
